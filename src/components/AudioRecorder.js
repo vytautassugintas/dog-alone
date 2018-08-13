e3fcc738ef7ac3089211203022516cf021316c15
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-08-14 00:14:02 +0300

    refactor: clean up

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -10,9 +10,7 @@ export class AudioRecorder extends Component {
     }
 
     this.audioCtx = new AudioContext();
-    this.analyser = this
-      .audioCtx
-      .createAnalyser();
+    this.analyser = this.audioCtx.createAnalyser();
     this.dataArray = []
   }
 
@@ -23,9 +21,7 @@ export class AudioRecorder extends Component {
 
     const audioChunks = [];
 
-    let source = this
-      .audioCtx
-      .createMediaStreamSource(this.stream);
+    let source = this.audioCtx.createMediaStreamSource(this.stream);
     source.connect(this.analyser)
 
     this.bufferLength = this.analyser.frequencyBinCount;
@@ -33,59 +29,41 @@ export class AudioRecorder extends Component {
 
     const timeSlice = 300;
 
-    this
-      .mediaRecorder
-      .start(timeSlice);
+    this.mediaRecorder.start(timeSlice);
 
-    this
-      .mediaRecorder
-      .addEventListener("dataavailable", event => {
-        audioChunks.push(event.data);
-        this.analyser.fftSize = 2048;
-        this
-          .analyser
-          .getByteTimeDomainData(this.dataArray);
+    this.mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+      this.analyser.fftSize = 2048;
+      this.analyser.getByteTimeDomainData(this.dataArray);
 
-        this.draw();
-      });
+      this.draw();
+    });
 
-    this
-      .mediaRecorder
-      .addEventListener("stop", () => {
-        const audioBlob = new Blob(audioChunks);
-        const audioUrl = URL.createObjectURL(audioBlob);
-        const audio = new Audio(audioUrl);
-        audio.play();
-      });
+    this.mediaRecorder.addEventListener("stop", () => {
+      const audioBlob = new Blob(audioChunks);
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    });
 
     setTimeout(() => {
-      this
-        .mediaRecorder
-        .stop();
+      this.mediaRecorder.stop();
     }, 3000);
 
     this.canvas = document.getElementById("oscilloscope");
-    this.canvasCtx = this
-      .canvas
-      .getContext("2d");
+    this.canvasCtx = this.canvas.getContext("2d");
   }
 
   draw() {
-    this
-      .analyser
-      .getByteTimeDomainData(this.dataArray);
+    this.analyser.getByteTimeDomainData(this.dataArray);
 
     this.canvasCtx.fillStyle = "rgb(200, 200, 200)";
-    this
-      .canvasCtx
-      .fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.canvasCtx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.canvasCtx.lineWidth = 2;
     this.canvasCtx.strokeStyle = "rgb(0, 0, 0)";
 
-    this
-      .canvasCtx
-      .beginPath();
+    this.canvasCtx.beginPath();
 
     let sliceWidth = this.canvas.width * 1.0 / this.bufferLength;
     let x = 0;
@@ -95,29 +73,20 @@ export class AudioRecorder extends Component {
       let y = v * this.canvas.height / 2;
 
       i === 0
-        ? this
-          .canvasCtx
-          .moveTo(x, y)
-        : this
-          .canvasCtx
-          .lineTo(x, y)
+        ? this.canvasCtx.moveTo(x, y)
+        : this.canvasCtx.lineTo(x, y)
 
       x += sliceWidth;
     }
 
-    this
-      .canvasCtx
-      .lineTo(this.canvas.width, this.canvas.height / 2);
-    this
-      .canvasCtx
-      .stroke();
+    this.canvasCtx.lineTo(this.canvas.width, this.canvas.height / 2);
+    this.canvasCtx.stroke();
   }
 
   render() {
     return (
       <div>
         <p>Audio controller</p>
-
       </div>
     )
   }
