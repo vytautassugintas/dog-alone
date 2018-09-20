08759a3689b080d2d20087c01d68740acedf1315
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-09-20 21:37:05 +0300

    chore: added chart.js

--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const SOCKET_HOST = process.env.SOCKET_HOST || 'http://localhost:3000';
+const SOCKET_HOST = process.env.SOCKET_HOST || 'http://ramplis.sytes.net:3000';
 
 const socket = openSocket(SOCKET_HOST);
 
