805a8e2f0a583032a4058751d5c1250e8966917e
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-11-14 23:14:59 +0200

    feat: select history chart time frame

--- src/components/HistoryChart.js
+++ src/components/HistoryChart.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import { Chart } from 'chart.js';
 import moment from 'moment';
 
+const createChartData = ({history = []}) => ({
+    datasets: [
+      {
+        label: 'dB level',
+        borderColor: '#f3a683',
+        color: '#ffffff',
+        // borderWidth: 1,
+        data: history.map(d => ({
+          t: d.date,
+          y: d.dbLevel.toFixed(2)
+        }))
+      }
+    ]
+})
+
 export class HistoryChart extends Component {
   constructor(props) {
     super(props);
+
+    this.onTimeFrameSelect = this.onTimeFrameSelect.bind(this);
 
     this.state = {};
   }
@@ -12,27 +29,43 @@ export class HistoryChart extends Component {
   componentDidMount() {}
 
   componentWillReceiveProps(props) {
-    const { history = [] } = props;
-
-    const data = {
-      datasets: [
-        {
-          label: 'dB level',
-          borderColor: '#f3a683',
-          color: '#ffffff',
-          // borderWidth: 1,
-          data: history.map(d => ({
-            t: d.date,
-            y: d.dbLevel.toFixed(2)
-          }))
-        }
-      ]
-    };
-
+    const data = createChartData(props)
     this.createChart(data);
   }
 
-  createChart(data) {
+  onTimeFrameSelect(event){
+    const { value } = event.target;
+    const data = createChartData(this.props)
+    const now = moment().hour();
+
+    const min = () => {
+      if(value === '24'){
+        return 0;
+      }
+      return now;
+    };
+
+    const max = () => {
+      if(now + Number.parseInt(value) > 24){
+        let maxTime = now + Number.parseInt(value);
+        while(maxTime !== 24) {
+          maxTime = maxTime - 1;
+        }
+        return maxTime;
+      }
+      return now + Number.parseInt(value);
+    }
+
+    const timeFrame = {
+      min: min(),
+      max: max()
+    }
+
+    this.createChart(data, timeFrame);
+    this.setState({value});
+  }
+
+  createChart(data, timeFrame = {min: 0, max: 24}) {
     const ctx = this.refs.historyCanvas.getContext('2d');
     new Chart(ctx, {
       type: 'line',
@@ -61,8 +94,8 @@ export class HistoryChart extends Component {
               distribution: 'linear',
               time: {
                 unit: 'hour',
-                min: moment({hour: 0}),
-                max: moment({hour: 24}),
+                min: moment({hour: timeFrame.min}),
+                max: moment({hour: timeFrame.max}),
               },
               ticks: {
                 fontSize: 12,
@@ -79,10 +112,18 @@ export class HistoryChart extends Component {
 
   render() {
     const { history } = this.props;
+    const { value } = this.state;
 
     return (
       <div className="box">
         <h3>HistoryChart</h3>
+        <span>Time frame </span>
+        <select onChange={this.onTimeFrameSelect} value={value}>
+          <option value="1">1 h</option>
+          <option value="3">3 h</option>
+          <option value="6">6 h</option>
+          <option value="24">24 h</option>
+        </select>
         <canvas ref="historyCanvas" id="myChart" />
       </div>
     );
